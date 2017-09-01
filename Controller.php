63a805b16dd35a1cc80e4950e40a3fb50d5c4e79
Kaempe_Laptop <Nikolajkaempe@hotmail.com>
2017-09-01 12:22:30 +0200

    removed login to check navigation

--- Controller.php
+++ Controller.php
@@ -22,11 +22,11 @@ class Controller
     }
     public function __construct()
     {
-        $User1 = new User(1, "Hejsae", "Password11", null, null);
-        $User2 = new User(2, "Jeffjeff", "Password12", null, null);
-        $User3 = new User(3, "OliverOli", "Password13", null, null);
-        $User4 = new User(4, "MarleneMM", "Password14", null, null);
-        $User5 = new User(5, "SusanneSS", "Password15", null, null);
+        $User1 = new User(1, 'Hejsae', 'Password11', null, null);
+        $User2 = new User(2, 'Jeffjeff', 'Password12', null, null);
+        $User3 = new User(3, 'OliverOli', 'Password13', null, null);
+        $User4 = new User(4, 'MarleneMM', 'Password14', null, null);
+        $User5 = new User(5, 'SusanneSS', 'Password15', null, null);
 
         $users = array($User1, $User2, $User3, $User4, $User5);
         $this->setUsers($users);
