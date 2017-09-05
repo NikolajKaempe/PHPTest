f032a49d9f68706e79830ab4a6b2fd93a0fe2bf3
Kaempe_Laptop <Nikolajkaempe@hotmail.com>
2017-09-05 11:07:19 +0200

    added css

--- Login.php
+++ Login.php
@@ -4,13 +4,14 @@
 
     <link rel="stylesheet" type="text/css" href="css/PrimaryStyle.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <?php include_once'css/PrimaryStyle.css'  ?>
 </head>
 
 <body>
 <form action="LoginController.php" method="post">
     <div class="container" style="margin-top: 30px; margin-bottom: 30px">
-        <h1 style="color: dodgerblue">Welcome!</h1>
-        <h2 style="color: dodgerblue">Please login to continue!</h2>
+        <h1>Welcome!</h1>
+        <h2>Please login to continue!</h2>
 
         <?php
             if(isset($_GET['error']))
@@ -19,11 +20,11 @@
             }
         ?>
 
-        <label style="color: dodgerblue" for="username">Username</label>
-        <input type="text" style="width: 50%; color: dodgerblue" name="username" id="username" class="form-control" placeholder="Username" required autofocus>
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" class="form-control" placeholder="Username" required autofocus>
         <br>
-        <label for="password" style="color: dodgerblue" class="sr-only">Password</label>
-        <input type="password" style="width: 50%; color: dodgerblue" name="password" id="password" class="form-control" placeholder="Password" required>
+        <label for="password" class="sr-only">Password</label>
+        <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
 
         <button style="margin-top: 30px; margin-bottom: 30px" class="btn btn-primary" type="submit">Sing in</button>
 
