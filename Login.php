d9ecdcae7a5bce9061b3df44edf5a820dcd5541a
Kaempe_Laptop <Nikolajkaempe@hotmail.com>
2017-09-05 11:11:43 +0200

    added css

--- Login.php
+++ Login.php
@@ -1,8 +1,7 @@
 <html>
 <head>
     <title>Login</title>
-
-    <link rel="stylesheet" type="text/css" href="css/PrimaryStyle.css">
+    <!-- <link rel="stylesheet" type="text/css" href="css/PrimaryStyle.css"> -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <style><?php include_once'css/PrimaryStyle.css'?></style>
 </head>
