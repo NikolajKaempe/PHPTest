422bf523726d80cfcfa083e7864d2db4d03ac1fb
Kaempe_Laptop <Nikolajkaempe@hotmail.com>
2017-09-05 10:40:53 +0200

    added css

--- Login.php
+++ Login.php
@@ -4,7 +4,6 @@
 
     <link rel="stylesheet" type="text/css" href="css/PrimaryStyle.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
 </head>
 
 <body>
@@ -16,16 +15,15 @@
         <?php
             if(isset($_GET['error']))
             {
-                echo '<label style="margin-left: 50px" class="label label-danger">'. $_GET['error'] . '</label> <br>';
+                echo '<label style="margin-left: 50px; color: dodgerblue"  class="label label-danger">'. $_GET['error'] . '</label> <br>';
             }
         ?>
 
         <label for="username">Username</label>
-        <input type="text" name="username" id="username" class="form-control" placeholder="Username" required autofocus>
+        <input type="text" style="width: 50%" name="username" id="username" class="form-control" placeholder="Username" required autofocus>
         <br>
         <label for="password" class="sr-only">Password</label>
-        <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
-
+        <input type="password" style="width: 50%" name="password" id="password" class="form-control" placeholder="Password" required>
 
         <button style="margin-top: 30px; margin-bottom: 30px" class="btn btn-primary" type="submit">Sing in</button>
 
